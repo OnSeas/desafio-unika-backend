3906a43cd91b16d8c2049fb3c84d24e50cc9d901
osmar <osmarneto750@gmail.com>
2024-03-04 13:48:24 -0300

    concertando erros de CORS policy

---
+++ src/main/java/com/unika/desafio/conifg/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.unika.desafio.conifg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+// CORS Policy - solução de: https://cursos.alura.com.br/forum/topico-configuracao-de-cors-spring-security-346607
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
